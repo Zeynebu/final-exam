b8a409727d5abbaa363ecaaecb733ae9a8901997
itman <0754914971michael@gmail.com>
2024-09-24 06:04:25 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:firebase_core/firebase_core.dart'; // Import Firebase core for initialization
+import 'login_page.dart';
+import 'shoe_app.dart'; // Your shoe app entry point
+import 'signup_page.dart'; // Import the Sign Up page
+import 'reset_password_page.dart'; // Import the Reset Password page
+import 'firebase_options.dart'; // Import your Firebase options
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform); // Initialize Firebase
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Shoe Store App',
+      theme: CupertinoThemeData(
+        primaryColor: CupertinoColors.activeBlue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/shoeApp': (context) => ShoeApp(), // Your shoe app main page
+        '/signup': (context) => SignUpPage(), // Route for Sign Up page
+        '/resetPassword': (context) => ResetPasswordPage(), // Route for Reset Password page
+      },
+    );
+  }
+}
